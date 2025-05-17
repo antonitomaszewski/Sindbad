663d62ffb73d96262a08e298b637686eb3f6d384
Antoni Tomaszewski <antek.tomaszewski@gmail.com>
2025-05-17 19:36:19 +0200

    Dodaje return po każdym res.status

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -8,15 +8,18 @@ export const registerUser = (req: Request, res: Response) => {
 
     if (!email || !password) {
         res.status(400).json({ message: 'Email i hasło są wymagane.' });
+        return;
     }
 
     const userExists = users.some((user: User) => user.email === email);
     if (userExists) {
         res.status(409).json({ message: 'Użytkownik o tym e-mailu już istnieje.' });
+        return;
     }
 
     users.push({ email, password });
     res.status(201).json({ message: 'Użytkownik zarejestrowany pomyślnie.' });
+    return;
 };
 
 
@@ -25,12 +28,15 @@ export const loginUser = (req: Request, res: Response) => {
 
     if (!email || !password) {
         res.status(400).json({ message: 'Email i hasło są wymagane.' });
+        return;
     }
 
     const user = users.find((user: User) => user.email === email && user.password === password);
     if (!user) {
         res.status(401).json({ message: 'Nieprawidłowy email lub hasło.' });
+        return;
     }
 
     res.status(200).json({ message: 'Zalogowano pomyślnie.' });
+    return;
 };
